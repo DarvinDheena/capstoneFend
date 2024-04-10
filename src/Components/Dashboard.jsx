7d3ce91013cca43353e9efa57de0add4bee6013c
DarvinDheena <darvindheena98@gmail.com>
2024-04-10 13:14:19 +0530

    navbar done

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -88,71 +88,67 @@ const  Dashboard =  ({ getUser })  => {
           {
             setTimeout(()=>{
               {
-                posts ? 
-                <> {
-                  posts.map(post=>{
-                  return(  
-                    <>
-                      <div className="d-flex align-items-center border-bottom border-secondary pb-2">
-                        <UserImage size = {'48px'} picturePath={ post.userPicturePath }/>
-                        <h5 key={ post._id } className='ms-3'>{post.firstName + ' ' + post.lastName }</h5>
-                      </div> 
-                      <div className='mt-4 border-bottom border-secondary pb-3  text-center'>
-                      <div>
-                        <p id='mydes' className='h-des mb-0 text-start' >{ post.description }</p>
+                posts.map(post=>{
+                return(  
+                  <>
+                    <div className="d-flex align-items-center border-bottom border-secondary pb-2">
+                      <UserImage size = {'48px'} picturePath={ post.userPicturePath }/>
+                      <h5 key={ post._id } className='ms-3'>{post.firstName + ' ' + post.lastName }</h5>
+                    </div> 
+                    <div className='mt-4 border-bottom border-secondary pb-3  text-center'>
+                    <div>
+                      <p id='mydes' className='h-des mb-0 text-start' >{ post.description }</p>
+                    </div>
+                      <img src={`${config.API_URL}/assets/${post.picturePath}`} className='img-fluid' alt="" key={posts._id}/>
+                    </div>
+                    <ul className='list-unstyled d-flex align-items-center mt-3'>
+                    <li  onClick={()=>handleLike(post._id)} key={post._id + "like"}>                  
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart-fill ms-2" viewBox="0 0 16 16" id={post._id}>
+                        <path  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                      </svg>
+                    </li>
+                    <li className='ms-4' key={post._id + "comment"}>
+                      <svg data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" xmlns="http://www.w3.org/2000/svg" width="29" height="29" fill="currentColor" className="bi bi-chat" viewBox="0 0 16 16" >
+                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
+                      </svg>
+                    </li>              
+                  </ul>
+                  <div className="accordion" id="accordionExample">
+                  <div className="accordion-item border-0">                  
+                    <div id="collapseOne" className="accordion-collapse collapse " data-bs-parent="#accordionExample">
+                      <div className="accordion-body">
+                      <div className="mb-3">
+                        <label  className="form-label">Add Comment</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='VIP' value={comment }
+                        onChange={ (e) =>setComment(e.target.value)  }></textarea>
+                        <button className='btn btn-primary mt-3' onClick={() => handleComment(post._id)}>send</button>
+                        <ul >
+                          {
+                            posts.map(post=>{
+                              const comments = post.comments ;
+                              return (
+                                <>
+                                    { comments.map((comment ,index)=> {
+                                    return(
+                                      <>
+                                        <li key={index}> { comment }</li>
+                                      </>)
+                                  })
+                                }
+                                </>
+                              )
+                            })
+                          }
+                        </ul>
                       </div>
-                        <img src={`${config.API_URL}/assets/${post.picturePath}`} className='img-fluid' alt="" key={posts._id}/>
-                      </div>
-                      <ul className='list-unstyled d-flex align-items-center mt-3'>
-                      <li  onClick={()=>handleLike(post._id)} key={post._id + "like"}>                  
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-heart-fill ms-2" viewBox="0 0 16 16" id={post._id}>
-                          <path  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-                        </svg>
-                      </li>
-                      <li className='ms-4' key={post._id + "comment"}>
-                        <svg data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" xmlns="http://www.w3.org/2000/svg" width="29" height="29" fill="currentColor" className="bi bi-chat" viewBox="0 0 16 16" >
-                          <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
-                        </svg>
-                      </li>              
-                    </ul>
-                    <div className="accordion" id="accordionExample">
-                    <div className="accordion-item border-0">                  
-                      <div id="collapseOne" className="accordion-collapse collapse " data-bs-parent="#accordionExample">
-                        <div className="accordion-body">
-                        <div className="mb-3">
-                          <label  className="form-label">Add Comment</label>
-                          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder='VIP' value={comment }
-                          onChange={ (e) =>setComment(e.target.value)  }></textarea>
-                          <button className='btn btn-primary mt-3' onClick={() => handleComment(post._id)}>send</button>
-                          <ul >
-                            {
-                              posts.map(post=>{
-                                const comments = post.comments ;
-                                return (
-                                  <>
-                                     { comments.map((comment ,index)=> {
-                                      return(
-                                        <>
-                                          <li key={index}> { comment }</li>
-                                        </>)
-                                    })
-                                  }
-                                  </>
-                                )
-                              })
-                            }
-                          </ul>
-                        </div>
-                        </div>
                       </div>
                     </div>
                   </div>
-                
-                    </>
-                      )
-                    })
-                  }
-                </> : <><h3> posts are loading ... please wait </h3> </>
+                </div>
+              
+                  </>
+                    )
+                  })
               }
             },2000)
           }    
